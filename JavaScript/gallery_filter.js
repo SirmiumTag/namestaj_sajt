263d7c31ca058c440e9b2e206d7bd65337646aba
Branislav Vucenovic <39958784+banexpro97@users.noreply.github.com>
2020-02-06 17:41:04 +0100

    Add files via upload

--- JavaScript/gallery_filter.js
+++ JavaScript/gallery_filter.js
@@ -1,12 +1,14 @@
 let metNamVar= document.getElementsByClassName("met_nam");
 let zidKrevVar= document.getElementsByClassName("zid_krev");
 let otiraciVar= document.getElementsByClassName("otiraci");
+let kancNamVar= document.getElementsByClassName("kanc_nam");
 let i;
 let metNamFun =()=>{
   for(i= 0; i< 3; i++){
     metNamVar[i].style.display= "block";
     zidKrevVar[i].style.display= "none";
     otiraciVar[i].style.display= "none";
+    kancNamVar[i].style.display= "none";
   }
   document.getElementById("left_pick").style.color = "#fbefe1";
   document.getElementById("left_pick").style.backgroundColor = "#54310c";
@@ -14,12 +16,15 @@ let metNamFun =()=>{
   document.getElementById("mid_pick").style.backgroundColor = "#fbefe1";
   document.getElementById("right_pick").style.color = "#54310c";
   document.getElementById("right_pick").style.backgroundColor = "#fbefe1";
+  document.getElementById("mid_pick_2").style.color = "#54310c";
+  document.getElementById("mid_pick_2").style.backgroundColor = "#fbefe1";
 }
 let otiraciFun =()=>{
   for(i= 0; i< 3; i++){
     metNamVar[i].style.display= "none";
     zidKrevVar[i].style.display= "none";
     otiraciVar[i].style.display= "block";
+    kancNamVar[i].style.display= "none";
   }
   document.getElementById("left_pick").style.color = "#54310c";
   document.getElementById("left_pick").style.backgroundColor = "#fbefe1";
@@ -27,12 +32,15 @@ let otiraciFun =()=>{
   document.getElementById("mid_pick").style.backgroundColor = "#54310c";
   document.getElementById("right_pick").style.color = "#54310c";
   document.getElementById("right_pick").style.backgroundColor = "#fbefe1";
+  document.getElementById("mid_pick_2").style.color = "#54310c";
+  document.getElementById("mid_pick_2").style.backgroundColor = "#fbefe1";
 }
 let zidKrevFun =()=>{
   for(i= 0; i< 3; i++){
     metNamVar[i].style.display= "none";
     zidKrevVar[i].style.display= "block";
     otiraciVar[i].style.display= "none";
+    kancNamVar[i].style.display= "none";
   }
   document.getElementById("left_pick").style.color = "#54310c";
   document.getElementById("left_pick").style.backgroundColor = "#fbefe1";
@@ -40,4 +48,22 @@ let zidKrevFun =()=>{
   document.getElementById("mid_pick").style.backgroundColor = "#fbefe1";
   document.getElementById("right_pick").style.color = "#fbefe1";
   document.getElementById("right_pick").style.backgroundColor = "#54310c";
+  document.getElementById("mid_pick_2").style.color = "#54310c";
+  document.getElementById("mid_pick_2").style.backgroundColor = "#fbefe1";
+}
+let kancNamFun =()=>{
+  for(i=0; i< 3; i++){
+    metNamVar[i].style.display= "none";
+    zidKrevVar[i].style.display= "none";
+    otiraciVar[i].style.display= "none";
+    kancNamVar[i].style.display= "block";
+  }
+  document.getElementById("left_pick").style.color = "#54310c";
+  document.getElementById("left_pick").style.backgroundColor = "#fbefe1";
+  document.getElementById("mid_pick").style.color = "#54310c";
+  document.getElementById("mid_pick").style.backgroundColor = "#fbefe1";
+  document.getElementById("right_pick").style.color = "#54310c";
+  document.getElementById("right_pick").style.backgroundColor = "#fbefe1";
+  document.getElementById("mid_pick_2").style.color = "#fbefe1";
+  document.getElementById("mid_pick_2").style.backgroundColor = "#54310c";
 }
